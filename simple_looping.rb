69eca8aac226aef46298d6f2633c3094f184d55f
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-13 22:21:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,6 +36,8 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for counter in 0..number_of_times do
+    puts phrase
+  end
 end
 
